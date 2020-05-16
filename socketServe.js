a968eb11b26ff3ff730ec561bbdae3db3cb7712b
lee <759757929@qq.com>
2020-05-17 00:57:50 +0800

    dfjkasgj

--- socketServe.js
+++ socketServe.js
@@ -4,47 +4,44 @@ const wss = new webSocket.Server({ port: 8081 });
 let receiveVal = [];
 let Data = [];
 let check = "";
-let status = 0
+let status = 0;
+let init = 1;
+let iniDate = [];
 const sqlTable = ["aircondition", "door", "heater", "lamp", "sound"];
 console.log("开始建立连接...");
+// 查询数据
 function sql(sql,message) {
   db.exec(`select * from ${sql} where name = ?`,[message],(err, data, fields) => {
       if (err) {
         console.log(err);
       } else {
-         Data = JSON.parse(JSON.stringify(data));
+         Data = JSON.parse(JSON.stringify(data));//查询所得数据
+         if(Data.length) {
+          iniDate = [...Data,...iniDate]//初始化数据
+         }
       }
     }
   );
 }
+//查询五个表数据，是否含有
 function forEachSql(message){
   sqlTable.forEach((item,index) => {
     sql(item,message);
-    if(Data.length >=1 && receiveVal[index]){
-      Data.forEach((data)=>{
-        if (receiveVal[index][data] != data) {
-          status = 1;
-        }
-      })      
-    } else{
-      status = 1;
-      receiveVal[index] = Data
-    }
+    receiveVal[index] = Data
   });
 }
 wss.on("connection", (client) => {
   client.on("message", (message) => {
-    // console.log("received: %s", message);
-    if(message){
-
-    }
     clearInterval(check);
     check = setInterval(() => {
       status = 0; 
       forEachSql(message)  
-      if (status == 1) {
+      if (status == 1 && !init) {
         client.send(JSON.stringify(Data));
         receiveVal = Data;
+      }else if(init){
+        //传输初始化数据
+        client.send(JSON.stringify(iniDate));
       }
     }, 1000);
   });
