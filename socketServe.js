7bf6e64e18aa4c4fee3708b80518ee0e5e82d859
lee <759757929@qq.com>
2020-05-17 14:05:19 +0800

    sadas

--- socketServe.js
+++ socketServe.js
@@ -11,53 +11,65 @@ let index = 1;
 const sqlTable = ["aircondition", "door", "heater", "lamp", "sound"];
 console.log("开始建立连接...");
 // 查询数据
-function sql(sql,message) {
-  db.exec(`select * from ${sql} where name = ?`,[message],(err, data, fields) => {
+function sql(sql, message) {
+  return new Promise((resolve, reject) => {
+    db.exec(`select * from ${sql} where name = ?`, [message], (err, data, fields) => {
       if (err) {
         console.log(err);
       } else {
-         Data = JSON.parse(JSON.stringify(data));//查询所得数据
-         console.log(Data,index++)
-         if(Data.length) {
-          iniDate = [...Data,...iniDate]//初始化数据
-         }
+        Data = JSON.parse(JSON.stringify(data));//查询所得数据
+        if (Data.length) {
+          iniDate = [...Data, ...iniDate]//初始化数据
+          console.log(iniDate, index++)
+        }
+        resolve()
       }
-    }
-  );
+    })
+  })
+
 }
 //查询五个表数据，是否含有
-function forEachSql(message){
-  sqlTable.forEach((item,index) => {
-    console.log(item,message)
-    sql(item,message);
-    //初始化后监听数据库的变化
-    // if(!ini && Data.length >=1){
-    //   Data[0].forEach(data=>{
-    //     //判断查询是否只有一项
-    //     if(Data.length == 1 ){
-    //       if(data != receiveVal[index][data]){
-    //         status = 1
-    //       }
-    //     }else{
-    //       if(data != receiveVal[index][data]){
-    //         status = 1
-    //       }
-    //     }
-    //   })
-  // }
-      receiveVal[index] = Data
+function forEachSql(message,client) {
+  sqlTable.forEach((item, index) => {
+    console.log(item, message)
+    sql(item, message).then(() => {
+      // 初始化后监听数据库的变化
+      //判断查询是否只有一项
+      if (!ini && Data.length == 1) {
+        for(let data in Data[0]){
+          if (data != receiveVal[index][0][data]) {
+            client.send(JSON.stringify(Data));
+            receiveVal[index] = Data
+            break;
+          }
+        }
+      } 
+      //查询有多项时
+      else {
+        Data.forEach((data,i) => {
+          for(let element in data){
+            if (element != receiveVal[index][i][element]) {
+              client.send(JSON.stringify(Data));
+              receiveVal[index] = Data
+              break;
+            }
+          }
+        })
+      }
+    });
+
   });
 }
 wss.on("connection", (client) => {
   client.on("message", (message) => {
     clearInterval(check);
     check = setInterval(() => {
-      status = 0; 
-      forEachSql(message)  
+      status = 0;
+      forEachSql(message,client)
       if (status == 1 && !init) {
         client.send(JSON.stringify(Data));
         receiveVal = Data;
-      }else if(init){
+      } else if (init) {
         //传输初始化数据
         client.send(JSON.stringify(iniDate));
         init = 0
