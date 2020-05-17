42639cfd08b82ae1c002bccb7967a351ea7fa6c8
wazy999 <52955313+wazy999@users.noreply.github.com>
2020-05-17 17:14:59 +0800

    :rocket:

--- socketServe.js
+++ socketServe.js
@@ -4,68 +4,71 @@ const wss = new webSocket.Server({ port: 8081 });
 let receiveVal = [];
 let Data = [];
 let check = "";
-let JsonData = [];
 let init = 1;
 const sqlTable = ["aircondition", "door", "heater", "lamp", "sound"];
 console.log("开始建立连接...");
-function sql(sql,message) {
-  db.exec(
-    `select * from ${sql} where name = ?`,
-    [message],
-    (err, data, fields) => {
-      if (err) {
-        console.log(err);
-      } else {
-        JsonData = JSON.parse(JSON.stringify(data));
+function sql(sql, message) {
+  return new Promise((resolve, reject) => {
+    db.exec(
+      `select * from ${sql} where name = ?`,
+      [message],
+      (err, data, fields) => {
+        if (err) {
+          console.log(err);
+        } else {
+          let JsonData = JSON.parse(JSON.stringify(data));
+          resolve(JsonData)
+        }
       }
-    }
-  );
+    );
+  })
 }
-function forEachSql(message){
-  Data = []
-  sqlTable.forEach((item,index) => {
-    sql(item,message);
-    if(JsonData.length == 1){
-      Data.push(JsonData[0]);
-    }else{
-      JsonData.forEach(data=>{
-        Data.push(data)
+function forEachSql(message,client) {
+    Data = []
+    sqlTable.forEach((item, index) => {
+      sql(item, message).then((res) => {
+        if (res.length == 1) {
+          Data.push(res[0]);
+        } else if (res.length >= 1) {
+          res.forEach(data => {
+            Data.push(data)
+          })
+        }
+        if (receiveVal.length == Data.length) {
+          receiveVal.forEach((item, index) => {
+            for (let element in item) {
+              if (receiveVal[index][element] != Data[index][element]) {
+                status = 1;
+              }
+            }
+          });
+        } else {
+          status = 1;
+        }
+        if (!init && status == 1) {
+          client.send(JSON.stringify(Data));
+          receiveVal = Data;
+        } else if (init) {
+          client.send(JSON.stringify(Data));
+          init = 0
+          receiveVal = Data;
+        }
       })
-    }
-  });
-}
+    });
+  }
 wss.on("connection", (client) => {
   client.on("message", (message) => {
-    if(message){
+    if (message) {
       init = 1
     }
     clearInterval(check);
     check = setInterval(() => {
-      let status = 0; 
-      forEachSql(message)  
-      if (receiveVal.length == Data.length) {
-        receiveVal.forEach((item, index) => {
-          for (let element in item) {
-            if (receiveVal[index][element] != Data[index][element]) {
-              status = 1;
-            }
-          }
-        });
-      } else {
-        status = 1;
-      }
-      if ( !init &&status == 1) {
-        client.send(JSON.stringify(Data));
-        receiveVal = Data;
-      }else if(init){
-        client.send(JSON.stringify(Data));
-        init = 0
-        receiveVal = Data;
-      }
-    }, 1000);
+      let status = 0;
+      forEachSql(message, client)}
+      ,1000);
+    });
+    client.on("close", () => {
+      clearInterval(check);
+      console.log("关闭服务器");
+    });
   });
-  client.on("close", () => {
-    clearInterval(check);
-    console.log("关闭服务器");
-  });
-});
